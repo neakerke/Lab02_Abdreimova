70583361a644aa46d6d3ce4cfd55500847ddd837
neakerke <103506349+neakerke@users.noreply.github.com>
2025-09-13 18:20:55 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-a=input("enter your IIN: ")
+import hashlib
 
-b = [int(c) for c in a]
+#iin = "020103512347"
+#i = 1
 
-# Проход 1: веса 1..11
-weights1 = list(range(1, 12))
-s1 = sum(d * w for d, w in zip(b, weights1))
-C1_int = s1 // 11
-k1 = s1 - C1_int * 11
+iin = input("vvedite iin - ")
+i = int(input("vvedite + - "))
 
-if k1 < 10:
-    control = k1
-else:
-    weights2 = [3,4,5,6,7,8,9,10,11,1,2]
-    s2 = sum(d * w for d, w in zip(b, weights2))
-    c2_int = s2 // 11
-    k2 = s2 - C2_int * 11
-    control = k2 if k2 < 10 else 0
+while True:
+    text = f"{iin}+{i}"
+    hash = hashlib.sha256(text.encode()).hexdigest()
 
-
-print("Razryad:", control)
+    if hash.startswith("00"):
+        print(text)
+        print(hash)
+        break
+    i += 1
