5f8288ea636541ac9a33f1ed21a918d62061df2c
neakerke <103506349+neakerke@users.noreply.github.com>
2025-09-18 19:23:41 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import hashlib
 
-#iin = "020103512347"
-#i = 1
+iin = "020103512876"
+i = 10
 
-iin = input("vvedite iin - ")
-i = int(input("vvedite + - "))
+#iin = input("vvedite iin - ")
+#i = int(input("vvedite + - "))
 
 while True:
     text = f"{iin}+{i}"
     hash = hashlib.sha256(text.encode()).hexdigest()
-
     if hash.startswith("00"):
-        print(text)
-        print(hash)
+        print("hash - ", text)
+        print("2 zeros - ", hash)
         break
     i += 1
+
+while True:
+    text = f"{iin}+{i}"
+    hash = hashlib.sha256(text.encode()).hexdigest()
+    if hash.startswith("000"):
+        print("hash - ", text)
+        print("3 zeros - ", hash)
+        break
+    i += 1
+
+
+
